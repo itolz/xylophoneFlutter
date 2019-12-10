45b92def07bcd3eae0dbc058fa892d61f606eb26
Italo Vinicios <italovinicios@gmail.com>
2019-12-09 22:18:36 -0300

    implementado funções Inclusive com widget)

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,42 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  Expanded buildKey({Color c , int soundNote}) {
+    return
+    Expanded(
+      child: FlatButton(
+        color: c,
+        onPressed: () {
+          playSound(soundNote);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-              child: FlatButton(
-            child: Text('Clique'),
-            onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+             buildKey(c: Colors.red, soundNote:1),
+             buildKey(c:Colors.yellow, soundNote:2),
+             buildKey(c:Colors.orange, soundNote:3),
+             buildKey(c:Colors.blue, soundNote:4),
+             buildKey(c:Colors.teal, soundNote:5),
+             buildKey(c:Colors.green, soundNote:6),
+             buildKey(c:Colors.purple, soundNote:7),
 
-            },
-          ))    ,
+            ],
+          ),
         ),
       ),
     );
